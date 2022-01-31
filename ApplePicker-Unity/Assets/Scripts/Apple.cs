04e063fe60e7d8c42275fddeefdf3c24014fb253
Yang <siy2217@utulsa.edu>
2022-01-31 16:45:36 -0600

    ApplePicker First Update

---
+++ ApplePicker-Unity/Assets/Scripts/Apple.cs
@@ -0,0 +1,30 @@
+/****
+ * Created By: Logan Patrick
+ * Date Created: 1/31/2022
+ * 
+ * Last Edited: N/A
+ * Last Edited by: N/A
+ * 
+ * Description: Controls tree movement and apple movement
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
